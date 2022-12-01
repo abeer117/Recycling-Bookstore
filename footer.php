ccdf1d9eb56d0afd3d7a024e74401bfa1dd9277a
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2022-12-01 18:48:59 +0600

    responsive media query added using javascript

--- footer.php
+++ footer.php
@@ -50,7 +50,105 @@
         </div>
     </div>
 </div>
+<script src="js\jquery-1.10.2.min.js" type="text/javascript"></script>
+<script src="js\bootstrap.min.js"></script>
+<script src="js\actions.js"></script>
+<!--okzoom Plugin-->
+<script src="js/okzoom.min.js" type="text/javascript"></script>
+<!--owl carousel plugin-->
+<script type="text/javascript" src="js/owl.carousel.js"></script>
 
+<script>
+    $(document).ready(function(){
+
+        $('#product-img').okzoom({
+            width: 200,
+            height: 200,
+            scaleWidth: 800
+        });
+
+        $('.banner-carousel').owlCarousel({
+            loop: true,
+            margin: 0,
+            responsiveClass: true,
+            navText : ["",""],
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+
+                },
+                600: {
+                    items: 1,
+                    nav: true
+                },
+                1000: {
+                    items: 1,
+                    nav: true,
+                    loop: false,
+                    margin: 10
+                }
+            }
+        });
+
+        $('.popular-carousel').owlCarousel({
+            loop: true,
+            margin: 0,
+            responsiveClass: true,
+            navText : ["",""],
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+
+                },
+                600: {
+                    items: 2,
+                    nav: true
+                },
+                800: {
+                    items: 4,
+                    nav: true
+                },
+                1000: {
+                    items: 5,
+                    nav: true,
+                    loop: false,
+                    margin: 10
+                }
+            }
+        });
+
+        $('.latest-carousel').owlCarousel({
+            loop: true,
+            margin: 0,
+            responsiveClass: true,
+            navText : ["",""],
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+
+                },
+                600: {
+                    items: 2,
+                    nav: true
+                },
+                800: {
+                    items: 3,
+                    nav: true
+                },
+                1000: {
+                    items: 4,
+                    nav: true,
+                    loop: false,
+                    margin: 5
+                }
+            }
+        });
+    });
+
+</script>
 
 </body>
 </html>
