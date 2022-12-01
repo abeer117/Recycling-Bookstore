34094adf2cba5d8ac3ecc6a8e56adb5a4746b265
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2022-12-01 17:24:15 +0600

    remove products from cart in actions.php

--- actions.php
+++ actions.php
@@ -28,3 +28,31 @@ if(isset($_POST['addCart'])){
     }
 }
 
+// remove products from cart
+if(isset($_POST['removeCartItem'])){
+    $p_id = $_POST['removeCartItem'];
+    
+    if($_COOKIE['cart_count'] == '1'){
+        setcookie('cart_count','',time() - (180),'/','','',TRUE);
+        setcookie('user_cart','',time() - (180),'/','','',TRUE);
+    }else{
+        if(isset($_COOKIE['user_cart'])){
+            $user_cart = json_decode($_COOKIE['user_cart']);
+            if(is_object($user_cart)){
+                $user_cart = get_object_vars($user_cart);
+            }
+            if (($key = array_search($p_id, $user_cart)) !== false) {
+                unset($user_cart[$key]);
+            }
+        }
+        $cart_count = count($user_cart);
+        $u_cart = json_encode($user_cart);
+
+        if(setcookie('user_cart',$u_cart,time() + (180),'/','','',TRUE)){
+            setcookie('cart_count',$cart_count,time() + (180),'/','','',TRUE);
+            echo 'cookie set successfully';
+        }else{
+            echo 'false';
+        }
+    }
+}
