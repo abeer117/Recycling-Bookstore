db85c4ab245bb81a2ddb8a621128a06f80240751
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2022-12-01 17:31:46 +0600

    bootstrap and custom font added

--- header.php
+++ header.php
@@ -8,3 +8,28 @@ if (!empty($header[0]['currency_format'])) {
     $cur_format = $header[0]['currency_format'];
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <?php if (isset($title)) { ?>
+        <title><?php echo $title; ?></title>
+    <?php } else { ?>
+        <title>Jarir Bookstore</title>
+    <?php } ?>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900|Montserrat:400,500,700,900" rel="stylesheet">
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <!-- Custom stlylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+</head>
