58574e601f44666c9b58105b0164978c032df0a8
JTVAI <112630971+JTVAI@users.noreply.github.com>
2022-12-04 03:42:09 +0600

    user order file bug fixed

--- user_orders.php
+++ user_orders.php
@@ -1,7 +1,7 @@
 <?php
 include 'config.php';
 session_start();
-if(isset($_SESSION['user_id']) && $_SESSION['user_role'] == 'user') {
+if(isset($_SESSION['user_id']) && $_SESSION['user_role'] == 'user')  {
 
     include 'header.php'; ?>
     <div class="product-cart-container">
